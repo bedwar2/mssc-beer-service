e780c48399f9112c28068df69b9f73b2db263357
Brian Edwards <bedwards@dcca.com>
2021-05-16 21:45:15 -0400

    Beer JPA ENtity

--- src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
@@ -1,6 +1,8 @@
 package guru.springframework.msscbeerservice.web.controller;
 
 import guru.springframework.msscbeerservice.web.model.BeerDto;
+import guru.springframework.msscbeerservice.web.model.BeerStyleEnum;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,12 +16,17 @@ public class BeerController {
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
 
-        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+        return new ResponseEntity<>(BeerDto.builder().beerName("Coors Light").id(beerId).upc(123L).beerStyle(BeerStyleEnum.LAGER).build(), HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto) {
-        return new ResponseEntity(HttpStatus.CREATED);
+        HttpHeaders headers = new HttpHeaders();
+        beerDto.setId(UUID.randomUUID());
+        //to do get hostname to url
+        headers.add("Location", "http://localhost:8080/api/v1/beer/" + beerDto.getId().toString());
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
@@ -27,4 +34,21 @@ public class BeerController {
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
+
+    @DeleteMapping("/{beerId}")
+    public ResponseEntity deleteBeerById(@PathVariable("beerId") UUID beerId)
+    {
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+
+    //Example returns BadRequest for Testing with Test Client
+    /*
+    @DeleteMapping("/{beerId}")
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public void deleteBeerById(@PathVariable("beerId") UUID beerId)
+    {
+        //return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+    */
 }
