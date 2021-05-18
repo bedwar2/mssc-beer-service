e991f4a823b85f336229dea9d0898b139990ed3a
Brian Edwards <bedwards@dcca.com>
2021-05-18 19:11:30 -0400

    Add JSON Format

--- src/main/java/guru/springframework/msscbeerservice/MsscBeerServiceApplication.java
+++ src/main/java/guru/springframework/msscbeerservice/MsscBeerServiceApplication.java
@@ -2,8 +2,10 @@ package guru.springframework.msscbeerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class MsscBeerServiceApplication {
 
 	public static void main(String[] args) {
